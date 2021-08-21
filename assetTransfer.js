cc1bbdf6784e3f44c9af544be37b1d5c54a3c8b7
Marija Vuco <48828437+mvuco00@users.noreply.github.com>
2021-08-21 20:34:46 +0200

    added GetAssetHistory

--- assetTransfer.js
+++ assetTransfer.js
@@ -1,9 +1,3 @@
-/*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 "use strict";
 
 const { Contract } = require("fabric-contract-api");
@@ -56,7 +50,6 @@ class AssetTransfer extends Contract {
     }
   }
 
-  // CreateAsset issues a new asset to the world state with given details.
   async CreateAsset(ctx, id, name, collage, grade) {
     const asset = {
       ID: id,
@@ -68,23 +61,20 @@ class AssetTransfer extends Contract {
     return JSON.stringify(asset);
   }
 
-  // ReadAsset returns the asset stored in the world state with given id.
   async ReadAsset(ctx, id) {
-    const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+    const assetJSON = await ctx.stub.getState(id);
     if (!assetJSON || assetJSON.length === 0) {
       throw new Error(`The asset ${id} does not exist`);
     }
     return assetJSON.toString();
   }
 
-  // UpdateAsset updates an existing asset in the world state with provided parameters.
   async UpdateAsset(ctx, id, name, collage, grade) {
     const exists = await this.AssetExists(ctx, id);
     if (!exists) {
       throw new Error(`The asset ${id} does not exist`);
     }
 
-    // overwriting original asset with new asset
     const updatedAsset = {
       ID: id,
       Name: name,
@@ -94,7 +84,6 @@ class AssetTransfer extends Contract {
     return ctx.stub.putState(id, Buffer.from(JSON.stringify(updatedAsset)));
   }
 
-  // DeleteAsset deletes an given asset from the world state.
   async DeleteAsset(ctx, id) {
     const exists = await this.AssetExists(ctx, id);
     if (!exists) {
@@ -103,13 +92,11 @@ class AssetTransfer extends Contract {
     return ctx.stub.deleteState(id);
   }
 
-  // AssetExists returns true when asset with given ID exists in world state.
   async AssetExists(ctx, id) {
     const assetJSON = await ctx.stub.getState(id);
     return assetJSON && assetJSON.length > 0;
   }
 
-  // TransferAsset updates the owner field of asset with given id in the world state.
   async TransferAsset(ctx, id, collage) {
     const assetString = await this.ReadAsset(ctx, id);
     const asset = JSON.parse(assetString);
@@ -117,10 +104,8 @@ class AssetTransfer extends Contract {
     return ctx.stub.putState(id, Buffer.from(JSON.stringify(asset)));
   }
 
-  // GetAllAssets returns all assets found in the world state.
   async GetAllAssets(ctx) {
     const allResults = [];
-    // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
     const iterator = await ctx.stub.getStateByRange("", "");
     let result = await iterator.next();
     while (!result.done) {
@@ -138,6 +123,46 @@ class AssetTransfer extends Contract {
       result = await iterator.next();
     }
     return JSON.stringify(allResults);
+  }
+
+  async GetAssetHistory(ctx, id) {
+    let resultsIterator = await ctx.stub.getHistoryForKey(id);
+    let results = await this._GetAllResults(resultsIterator, true);
+
+    return JSON.stringify(results);
+  }
+
+  async _GetAllResults(iterator, isHistory) {
+    let allResults = [];
+    let res = await iterator.next();
+    while (!res.done) {
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        console.log(res.value.value.toString("utf8"));
+        if (isHistory && isHistory === true) {
+          jsonRes.TxId = res.value.txId;
+          jsonRes.Timestamp = res.value.timestamp;
+          try {
+            jsonRes.Value = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Value = res.value.value.toString("utf8");
+          }
+        } else {
+          jsonRes.Key = res.value.key;
+          try {
+            jsonRes.Record = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Record = res.value.value.toString("utf8");
+          }
+        }
+        allResults.push(jsonRes);
+      }
+      res = await iterator.next();
+    }
+    iterator.close();
+    return allResults;
   }
 }
 
